56939f31d3947c3b37a7c9823fb8ecfefad64d6b
kevinmistrele <kevintavaresmistrele@gmail.com>
2026-02-16 09:26:48 -0300

    feat: add setInitialized reducer to manage app initialization state

--- src/application/store/slices/app-slice.ts
+++ src/application/store/slices/app-slice.ts
@@ -8,12 +8,18 @@ const initialState: AppState = {
   isInitialized: true,
 };
 
+// App slice is responsible for managing the global state of the application, such as initialization status.
+// It provides a reducer to update the state and an action to set the initialization status.
+// This slice is used in the Redux store to maintain and access the app's state throughout the application.
 export const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
-    // Reducers vazios por enquanto, apenas para satisfazer o Store
+    setInitialized: (state, action) => {
+      state.isInitialized = action.payload;
+    },
   },
 });
 
-export default appSlice.reducer;
+export const { setInitialized } = appSlice.actions;
+export default appSlice.reducer; // O appReducer que a Store procura é este default export!
